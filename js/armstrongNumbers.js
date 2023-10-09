b642d23461faa8c5e8498f85da95e322afc1c401
Joshua Minchew <jminchew97@gmail.com>
2023-10-09 10:58:22 -0500

    finished

--- js/armstrongNumbers.js
+++ js/armstrongNumbers.js
@@ -1,5 +1,30 @@
 // How can you make this more scalable and reusable later?
 
-exports.findArmstrongNumbers = function() {
-
+exports.findArmstrongNumbers = function(numsList) {
+    let l = []
+    for (let i of numsList){
+        let strNum = i.toString()
+        let len = strNum.length
+        
+        let total = 0
+        if (len == 3){
+            total =Number(strNum[0])**Number(len) + Number(strNum[1])**Number(len) + Number(strNum[2])**Number(len)
+        }else if(len == 2){
+            total =Number(strNum[0])**Number(len) + Number(strNum[1])**Number(len)
+        }else if(len == 1){
+            total = Number(strNum[0])**Number(len)
+        }
+        
+        if (total == Number(i)){
+            l.push(i)
+        }
+    }
+    return l
 };
+// console.log(findArmstrongNumbers([1,2,3,4,5,6,7,8,9,10,11,12,13,14]))
+
+// [
+//     0,   1, 2, 3,   4,   5,
+//     6,   7, 8, 9, 153, 370,
+//   371, 407
+// ]
--- python/armstrong_numbers.py
+++ python/armstrong_numbers.py
@@ -1,2 +1,16 @@
 # How can you make this more scalable and reusable later?
 def find_armstrong_numbers(numbers):
+    l = []
+    for num in numbers:
+        length = len(str(num))
+        numStr = str(num)
+        
+        total = 0
+        for dig in numStr:
+            total += int(dig)**int(length)
+
+        if total == num:
+            l.append(num)
+    return l
+
+# print(find_armstrong_numbers([1,2,3,4,6,6,371]))
